8ee389c9b625cb9fd7b63e7ef3d1d73191aae69a
wilmerortiz <jhak_dy@hotmail.com>
2023-04-30 17:29:49 -0500

    search

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Container from '@/app/components/Container'
+import Logo from '@/app/components/navbar/Logo'
+import Search from '@/app/components/navbar/Search'
+import UserMenu from '@/app/components/navbar/UserMenu'
+
+import { SafeUser } from '@/app/types'
+import Categories from '@/app/components/navbar/Categories'
+
+interface NavbarProps {
+  currentUser?: SafeUser | null
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  return (
+    <div className='fixed w-full bg-white z-10 shadow-sm'>
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      <Categories />
+    </div>
+  )
+}
+
+export default Navbar
